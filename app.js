3315f99beb689cb48f20fba008bc48ef6c6a80ea
Ryan Hood <ryanhood@Ryans-MacBook-Pro.local>
2024-03-28 23:44:32 -0500

    set up for api call

--- app.js
+++ app.js
@@ -13,6 +13,32 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.post('/subscribe', (req, res) => {
     const { email,js } = req.body;
     console.log(req.body);
+
+    const mcData = {
+        members: [
+            {
+                email_address: email,
+                status: 'subscribed'
+            }
+        ]
+    }
+
+    const mcDataPosts = JSON.stringify(mcData);
+
+    const options = {
+        url: '',
+        method: 'POST',
+        headers: {
+            Authorization: 'auth ..'
+        },
+        body: mcDataPosts
+    }
+
+    if(email){
+        //successful so far
+    } else {
+        res.status(404).send({message: 'Failed'})
+    }
 })
 
 const PORT = process.env.PORT || 8800;
